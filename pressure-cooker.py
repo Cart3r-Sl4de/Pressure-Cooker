b5e37ad30d434e978434ea2154928d02e0b7fa45
Carter Slade <CarterSlade7@protonmail.com>
2023-08-27 20:30:04 -0700

    Edification
    Added more funcitonality with the lively pressure cooker, and some prompt updates. More edifications to come!
    
    Thank you for reading!

--- pressure-cooker.py
+++ pressure-cooker.py
@@ -1,13 +1,24 @@
-import pyautogui, time
+import pyautogui, time, sys
 from pathlib import Path
 
 sign = "██████████████████████████████████████████████████████████████████████████████████████\n█▄─▄▄─█▄─▄▄▀█▄─▄▄─█─▄▄▄▄█─▄▄▄▄█▄─██─▄█▄─▄▄▀█▄─▄▄─███─▄▄▄─█─▄▄─█─▄▄─█▄─█─▄█▄─▄▄─█▄─▄▄▀█\n██─▄▄▄██─▄─▄██─▄█▀█▄▄▄▄─█▄▄▄▄─██─██─███─▄─▄██─▄█▀███─███▀█─██─█─██─██─▄▀███─▄█▀██─▄─▄█\n▀▄▄▄▀▀▀▄▄▀▄▄▀▄▄▄▄▄▀▄▄▄▄▄▀▄▄▄▄▄▀▀▄▄▄▄▀▀▄▄▀▄▄▀▄▄▄▄▄▀▀▀▄▄▄▄▄▀▄▄▄▄▀▄▄▄▄▀▄▄▀▄▄▀▄▄▄▄▄▀▄▄▀▄▄▀"
 
 def pressureCooker(nightmare):
 
-    permutations = int(input("How many times do you wish to repeat this input? "))
+    while True:
+        clickOrEnter = input("Do you want the program to enter the input by pressing\na)enter\nb)click\n[?] ")
+        if clickOrEnter == "a" or clickOrEnter == "b":
+            break
+        else:
+            print("ERROR DETECTED: incorrect input. Please input a or b\n\n")
+
+    
+
+
+    permutations = int(input("How many times do you wish to repeat this input?\n[?] "))
     permutations = 1 if permutations < 0 else permutations
-    finality = input("\nLast chance to change your mind!\nIf you enter \"y\" you will proceed and will have 12 seconds to select the text box of which you wish to enter into!\nEnter anything else to exit: ")
+    timing = int(input("Should there be a delay with the repeated input? If so, enter any amount of seconds. If not, just type 0\n[?] "))
+    finality = input("\nLast chance to change your mind!\nIf you enter \"y\" you will proceed and will have 12 seconds to select the text box of which you wish to enter into!\nEnter anything else to exit:\n[?] ")
 
     if finality == "y":
         print("Hop to it! The 12 seconds start now!")
@@ -20,14 +31,24 @@ def pressureCooker(nightmare):
                 for x in range(permutations):
                     for line in file_location.open("r"):
                         pyautogui.typewrite(line)
-                        pyautogui.press("enter")
-            except:
-                print("problem is with the loop")
+                        if clickOrEnter == "a":
+                            pyautogui.press("enter")
+                        elif clickOrEnter == "b":
+                            pyautogui.click()
+                        time.sleep(timing)
+            except Exception as e:
+                print(f"ERROR DETECTED: {e}\n\n")
+                sys.exit()
 
         else:
             for x in range(permutations):
                 pyautogui.typewrite(nightmare)
-                pyautogui.press("enter")
+                if clickOrEnter == "a":
+                    pyautogui.press("enter")
+                elif clickOrEnter == "b":
+                    pyautogui.click()
+
+                time.sleep(timing)
 
     else:
         print("Understandable, have a good day")
@@ -38,7 +59,7 @@ def main():
 
     while(True):
         try:
-            query = input("I must ask, do you wish to use input from (type corresponding letter):\na)typing it in\nb)from text file\n")
+            query = input("I must ask, do you wish to use input from (type corresponding letter):\na)typing it in\nb)from text file\n[?] ")
 
             if query == "a":
                 query = input("Lively, type below your desired query:\n")
@@ -49,10 +70,15 @@ def main():
                 pressureCooker("txtfile")
 
             else:
-                print("Exiting due to typing something other than listed inputs.\nUnderstandable, have a good day!")
+                print("\nEXITING DUE TO: typing something other than listed inputs.\nUnderstandable, have a good day!")
                 time.sleep(7)
                 
             break
+
+        except KeyboardInterrupt:
+            print("\nEXITING DUE TO: User Termination")
+            time.sleep(5)
+            sys.exit()
 
         except:
             print("ERROR DETECTED: Incorrect Input type.\nI kindly ask you apply the correct type of input when prompted!\n\n")
